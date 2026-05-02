16c7279c6bb7f037ad376b5ea7caadc9f8f2b5fb
Dagger <496642325@qq.com>
2026-05-02 09:36:56 +0800

    d

--- test.py
+++ test.py
@@ -1,17 +1,23 @@
 import ocr
 import json
 from pathlib import Path
-#import huggingface_hub
+import huggingface_hub
+import sys
 import yaml
+
+not_network = False
+if len(sys.argv) > 1 and sys.argv[1] == "--not-network":
+    not_network = True
 
 with open("conf.yaml", "r", encoding="utf-8") as f:
     conf = yaml.safe_load(f)
 
-#huggingface_hub.login(conf["key"]["hf_token"])
+if not not_network:
+    huggingface_hub.login(conf["key"]["hf_token"])
 
 TEST_DATA_ROOT = Path(__file__).parent / "test_data"
 
-mocr = ocr.MangaOcr(local_files_only=False,force_cpu=True)
+mocr = ocr.MangaOcr(local_files_only=not_network,force_cpu=True)
 
 expected_results = json.loads((TEST_DATA_ROOT / "expected_results.json").read_text(encoding="utf-8"))
 
